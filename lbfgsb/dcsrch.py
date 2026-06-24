8bd18597e4e217671b48a500c7b73fd142c1efa2
antoine.collet <antoine.collet@orano.group>
2026-06-24 12:57:32 +0800

    FIX: use typing_extensions.py

--- lbfgsb/dcsrch.py
+++ lbfgsb/dcsrch.py
@@ -21,7 +21,7 @@ from __future__ import annotations
 
 import math
 from dataclasses import dataclass
-from typing import Literal
+from typing_extensions import Literal
 
 Task = Literal["FG", "CONV", "WARN", "ERROR"]
 
